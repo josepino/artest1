16f48c84920cbdcbdd0abd3d4261e507a9362869
José Pino <joselopino@gmail.com>
2017-12-12 17:05:35 -0300

    Cubo funcionando en AR. Aparece enfrente de la cámara.

--- ARTest/ViewController.swift
+++ ARTest/ViewController.swift
@@ -19,26 +19,49 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+    
+        // Creamos el cubo
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        // Creamos el material
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // Creamos el nodo y ponemos su posicion en el espacio XYZ
+        let node = SCNNode()
+        node.position = SCNVector3(x:0, y:1, z:-0.5)  // El eje Y es para subir un poquito y Z negativo es para que salga enfrente
+        node.geometry = cube
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        
+        // Colocamos el nodo en la escena
+        // Un nodo puede tener varios hijos nodos
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        // Le agreamos sombras y luces
+        sceneView.autoenablesDefaultLighting = true
+        
+        
+        
+
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
-        sceneView.session.run(configuration)
+            // Create a session configuration
+            let configuration = ARWorldTrackingConfiguration()
+            // Run the view's session
+            sceneView.session.run(configuration)
+        
     }
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -46,35 +69,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
 
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
+
